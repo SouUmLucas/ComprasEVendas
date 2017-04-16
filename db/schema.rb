1696832706645a9c901ee20e71f2049c54c82a6e
LAPTOP-J51E4L8O\Lucas Santos <lucas.santos.silva2011@gmail.com>
2017-04-16 13:56:05 -0300

    Upgrade app to Rails version 5.0.2

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -34,6 +33,7 @@ ActiveRecord::Schema.define(version: 20160619014834) do
 
   create_table "products", force: :cascade do |t|
     t.string   "description"
+    t.string   "type"
     t.decimal  "price"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
